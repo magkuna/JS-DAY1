697f2fbb3cccdc39f767256f45439bdd578f7586
magkuna <magkuna@gmail.com>
2019-03-11 11:26:58 +0100

    inkrementacja

--- 01/script.js
+++ 01/script.js
@@ -5,3 +5,17 @@ var a= [1,2,3,4];
 var b= { a: 1, b: 'kot', c:45}
 
 console.log(typeof x,typeof y, typeof z,typeof a,typeof b);
+
+
+var f = 2;
+var g = 4;
+f+=f;
+g-=g;
+
+console.log(f,g);
+
+var h = 2;
+console.log (h--);
+console.log(h);
+console.log(--h);
+console.log(h);

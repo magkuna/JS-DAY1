545c9e9f53f16063137fe427f199f4c13e380308
magkuna <magkuna@gmail.com>
2019-03-11 13:23:24 +0100

    if excercise

--- 01/script.js
+++ 01/script.js
@@ -1,4 +1,4 @@
-var x=5;
+/*var x=5;
 var y='ala';
 var z=false;
 var a= [1,2,3,4];
@@ -38,4 +38,51 @@ console.log(!(j==k));
 console.log(j==!k);
 console.log(!j==k); 
 console.log( !j==!k);
+
+
+
+var a = 5;
+var b = '5';
+var c = [1];
+var d = 'ala';
+var e = '30';
+var f = b;
+var g = 30;
+var h = '30';
+var i = 30;
+
+console.log(d>e);
+console.log (a==b);
+console.log (g ===h);
+console.log (g ==h)
+console.log(g ===i);
+console.log (g == i); 
+
+var czyJestCieplo = false;
+var czySwieciSlonce = true;
+
+console.log (czyJestCieplo || czySwieciSlonce);
+console.log (!czyJestCieplo || czySwieciSlonce);
+console.log (czyJestCieplo || !czySwieciSlonce);
+console.log (!czyJestCieplo || !czySwieciSlonce);
+
+console.log (czyJestCieplo && czySwieciSlonce);
+console.log (!czyJestCieplo && czySwieciSlonce);
+console.log (czyJestCieplo && !czySwieciSlonce);
+console.log (!czyJestCieplo && !czySwieciSlonce); */
+
+
+var a = 1;
+if (a===1){
+    console.log(1)
+}
+else if ( a === '1'){
+    console.log(2)
+}
+else if (a===2){
+    console.log(a)
+}
+else {
+    console.log(0)
+}
 

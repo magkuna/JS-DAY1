5b1d80c11d9762283c6f251e53d78158c00e5150
magkuna <magkuna@gmail.com>
2019-03-12 13:24:16 +0100

    fir exercise

---
+++ objects/script.js
@@ -0,0 +1,9 @@
+//var myCar = new {} linijka ta sama jak druga tylko ze inny zapis
+var myDogs = new Object();
+
+myDogs.first = 'Atos';
+myDogs.second = 'Mika';
+myDogs.third='Futi';
+
+console.log(myDogs);
+
